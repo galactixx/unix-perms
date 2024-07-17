8e4bff7ba35f191061e09f4e9bf8e522000af4f0
galactixx <nwlezien.github@gmail.com>
2024-07-16 22:21:11 -0500

    bump version to 0.4.0

--- unix_perms/__init__.py
+++ unix_perms/__init__.py
@@ -6,7 +6,7 @@ from unix_perms.permissions import OctalPermissions
 from unix_perms.types import (PermissionsByte, PermissionsConfig,
                               PermissionsMode)
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __all__ = [
     'InvalidOctalError',
     'OctalPermissions',
